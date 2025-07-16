a6019a17df451f6eae888effb5b16d20af3bc659
robergit88 <86879958+robergit88@users.noreply.github.com>
2025-07-16 16:00:50 +0200

    dd

--- Library/js/main.js
+++ Library/js/main.js
@@ -1,3 +1,13 @@
-function saludar() {
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+function agregar() {
     alert("¡Hola, bienvenido a la biblioteca!");
 }
