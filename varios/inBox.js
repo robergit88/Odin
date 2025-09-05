e5074139688ceddd3a906ba1071758041597668b
robergit88 <86879958+robergit88@users.noreply.github.com>
2025-09-05 10:20:10 +0200

    ff

--- varios/inBox.js
+++ varios/inBox.js
@@ -1,4 +1,12 @@
+// La caja tiene un regalo (*) y cuenta como dentro de la caja si:
+// Está rodeada por # en los bordes de la caja.
+// El * no está en los bordes de la caja.
+
 function inBox(box) {
+
+    if (box.join().indexOf("*") < 0) {
+        return console.log(false);
+    }
 
     for (let i = 0; i < box.length; i++) {
 
@@ -8,17 +16,47 @@ function inBox(box) {
 
         if (pos > 0) {
 
-            if (element.charAt(0) == "#" &&
+            // if (element.charAt(0) == "#" &&
+            if (element.startsWith("#") &&
                 element.endsWith("#")) {
-                console.log(true)
+                console.log(true);
+
             }
+            else {
+
+                console.log(false);
+            }
+
+            break
         }
     }
 
 }
 
+// inBox([
+//     "###",
+//     "#*#",
+//     "###"
+// ])
+
+// inBox([
+//     "####",
+//     "#* #",
+//     "#  #",
+//     "####"
+// ]) // ➞ true
+
 inBox([
-    "###",
-    "#*#",
-    "###"
-])
+    "#####",
+    "#   #",
+    "#  #*",
+    "#####"
+]) // ➞ false
+
+// inBox([
+//     "#####",
+//     "#   #",
+//     "#   #",
+//     "#   #",
+//     "#####"
+// ]) // ➞ false
