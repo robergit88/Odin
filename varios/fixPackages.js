1123c18cf86d47840f456cb1707584b7ab722e8f
robergit88 <86879958+robergit88@users.noreply.github.com>
2025-09-05 11:42:46 +0200

    kk

---
+++ varios/fixPackages.js
@@ -0,0 +1,8 @@
+function fixPackages(packages) {
+
+}
+
+// fixPackages('a(cb)de')
+// fixPackages('a(bc(def)g)h')
+// fixPackages('abc(def(gh)i)jk')
+// fixPackages('a(b(c))e')
--- varios/varios.js
+++ varios/varios.js
@@ -1,18 +1,24 @@
-// Primero ejecuta en terminal: npm install prompt-sync
-const prompt = require('prompt-sync')();
+// let text = "This (is) W3School";
+// // let pattern = /[A-Z]/g; //buscar letras del alfabeto en IMPRENTA
 
-function contrasenaValida(valor) {
-    if (valor === '2Fj(jjbFsuj' || valor === 'eoZiugBf&g9') {
-        console.log("✅ Contraseña válida.");
-        return true;
-    } else {
-        console.log("❌ Contraseña inválida.");
-        return false;
-    }
-}
+// let text = "Hellooo World! Hello W3Schools!";
+// let pattern = /lo*/g;
 
-// Pedir contraseña
-const contrasena = prompt('Ingrese su contraseña: ');
-contrasenaValida(contrasena);
+// let text = "Is this (all) there is?";
+// let pattern = /is/g;
 
+// let text = "This is W3School";
+// let pattern = / is /g;
+// // let pattern = /[^W]/g;
 
+// let text = "That's hot!";
+// let pattern = /h.t/g; //[ 'hat', 'hot' ]
+
+// let text = "This (is) W3School";
+// let pattern = /(.)/g;
+
+let text = "HELLO, LOOK AT YOU";
+let pattern = /\bLO/;
+
+let result = text.match(pattern);
+console.log(result)
